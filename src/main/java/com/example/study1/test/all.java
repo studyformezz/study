ee9a5becbee93d328dddaecd22d384e721a339cb
qing.zhang <qing.zhang@tongdun.net>
2022-07-26 23:07:27 +0800

    20220726

---
+++ src/main/java/com/example/study1/test/all.java
@@ -0,0 +1,4 @@
+package com.example.study1.test;
+
+public class all {
+}
---
+++ src/main/java/com/example/study1/test/test.java
@@ -0,0 +1,8 @@
+package com.example.study1.test;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello word ,前后端分离项目练习题1，正式开始");
+    }
+    //qwe
+}
